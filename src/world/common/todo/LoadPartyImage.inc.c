ae687240a01c1507e780667d28bf8ab134a88fec
Bassel Shmali <bass3l@live.com>
2026-09-17 07:49:48 +0200

    fix partner images dangling buffer

--- src/world/common/todo/LoadPartyImage.inc.c
+++ src/world/common/todo/LoadPartyImage.inc.c
@@ -10,17 +10,15 @@
 #define PARTY_IMAGE_WIDTH 150
 #define PARTY_IMAGE_HEIGHT 105
 
+#define PARTY_IMAGE_PATH "__OTR__party/" PARTY_IMAGE
+#define PARTY_IMAGE_PAL_PATH PARTY_IMAGE_PATH "_pal"
+
 API_CALLABLE(N(LoadPartyImage)) {
     static MessageImageData image;
 
-    char palPath[64];
-    char imgPath[64];
-    snprintf(palPath, sizeof(palPath), "__OTR__party/%s_pal", PARTY_IMAGE);
-    snprintf(imgPath, sizeof(imgPath), "__OTR__party/%s", PARTY_IMAGE);
-
     // Drawn by name where the archive has the portrait as a texture
-    image.palette = (PAL_BIN*)port_named_image(imgPath, "_img_tlut", LOAD_ASSET(palPath));
-    image.raster = (IMG_BIN*)port_named_image(imgPath, "_img", LOAD_ASSET(imgPath));
+    image.palette = (PAL_BIN*)port_named_image(PARTY_IMAGE_PATH, "_img_tlut", LOAD_ASSET(PARTY_IMAGE_PAL_PATH));
+    image.raster = (IMG_BIN*)port_named_image(PARTY_IMAGE_PATH, "_img", LOAD_ASSET(PARTY_IMAGE_PATH));
     image.width = PARTY_IMAGE_WIDTH;
     image.height = PARTY_IMAGE_HEIGHT;
     image.format = G_IM_FMT_CI;
@@ -29,4 +27,6 @@ API_CALLABLE(N(LoadPartyImage)) {
     return ApiStatus_DONE2;
 }
 
+#undef PARTY_IMAGE_PAL_PATH
+#undef PARTY_IMAGE_PATH
 #undef PARTY_IMAGE
